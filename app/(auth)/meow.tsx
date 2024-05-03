f8c9483947d8945c7c8545541bd4ceb10728a228
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2024-05-03 14:31:38 +0530

    update: chatting works

--- app/(auth)/meow.tsx
+++ app/(auth)/meow.tsx
@@ -1,43 +1,144 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { View, FlatList, Button, StyleSheet, Text } from "react-native";
-import { Appbar } from "react-native-paper";
+import { Appbar, Icon, MD3Colors, withTheme } from "react-native-paper";
 import { TextInput } from "react-native-paper";
 import { Platform } from "react-native";
 import database from "../../firebaseConfig";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { useUser } from "@clerk/clerk-expo";
-import { ref, set, push, serverTimestamp, onValue } from "firebase/database";
+
+import {
+  ref,
+  set,
+  push,
+  serverTimestamp,
+  onValue,
+  onChildAdded,
+} from "firebase/database";
 import { Message } from "../types/MessageTypes";
-import { GiftedChat } from "react-native-gifted-chat";
-const Meow = () => {
+import {
+  Bubble,
+  GiftedChat,
+  InputToolbar,
+  Send,
+} from "react-native-gifted-chat";
+const Meow = ({ theme }) => {
   const { user } = useUser();
 
   const [newMessage, setNewMessage] = useState("");
   const MORE_ICON = Platform.OS === "ios" ? "dots-horizontal" : "dots-vertical";
   const chatId = "chat1"; // For simplicity, we are using a hardcoded chatId
-  const messageRef = ref(database, `chats/${chatId}/allnew`);
+  const messageRef = ref(database, `chats/${chatId}/987654325`);
   const [messages, setMessages] = useState([]);
   useEffect(() => {
-    onValue(messageRef, (snapshot) => {
-      const data = snapshot.val();
+    onChildAdded(messageRef, (data) => {
       if (data) {
-        const messages = Object.values(data);
-        const messagesExceptLast = messages;
-        setMessages(messagesExceptLast);
+        console.log("Data", data);
+        setMessages((previousMessages) =>
+          GiftedChat.append(previousMessages, data.val())
+        );
       }
     });
   }, []);
-
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      backgroundColor: theme.colors.secondaryContainer,
+      backgroundImage: "url('https://source.unsplash.com/random')",
+    },
+    messageContainer: {
+      backgroundColor: "#ffffff",
+      borderRadius: 8,
+      marginVertical: 5,
+      marginHorizontal: 10,
+      padding: 10,
+      maxWidth: "70%",
+    },
+    messageText: {
+      fontSize: 15,
+    },
+    timestamp: {
+      fontSize: 15,
+      color: "gray",
+      marginTop: 5,
+      alignSelf: "flex-end",
+    },
+    inputContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      borderTopWidth: 1,
+      borderTopColor: "#cccccc",
+      backgroundColor: "#ffffff",
+      overflow: "hidden",
+    },
+    input: {
+      borderWidth: 1,
+      marginHorizontal: 10,
+      marginBottom: 10,
+      borderColor: "#cccccc",
+      borderTopEndRadius: 20,
+      borderTopStartRadius: 20,
+      borderBottomEndRadius: 20,
+      borderBottomStartRadius: 20,
+      overflow: "hidden",
+      borderBottom: 0,
+    },
+  });
   const onSend = (messages = []) => {
     messages.forEach((message) => {
-      // Send the message to the backend
+      console.log("Yet to send Message", message);
+      message.createdAt = serverTimestamp();
       push(messageRef, message);
+
       console.log("Message sent", message);
     });
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, messages)
+  };
+  const customSystemMessage = (props) => {
+    return (
+      <View style={{}}>
+        <Text style={{}}>
+          Your chat is secured. Remember to be cautious about what you share
+          with others.
+        </Text>
+      </View>
     );
   };
 
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: theme.colors.onSecondaryContainer,
+          },
+        }}
+        textStyle={{
+          right: {
+            color: theme.colors.text,
+          },
+        }}
+      />
+    );
+  };
+  const renderSend = (props) => {
+    return (
+      <Send {...props}>
+        <MaterialCommunityIcons
+          name="send-circle"
+          style={{ marginBottom: 5, marginRight: 5 }}
+          size={32}
+          color={theme.colors.primary}
+        />
+      </Send>
+    );
+  };
+  const scrollToBottomComponent = () => {
+    return <Icon source="down" color={theme.colors.primary} size={32} />;
+  };
   return (
     <View style={styles.container}>
       <Appbar.Header>
@@ -47,16 +148,20 @@ const Meow = () => {
       </Appbar.Header>
       <GiftedChat
         messages={messages}
+        renderSystemMessage={customSystemMessage}
         //showAvatarForEveryMessage={true}
         //showUserAvatar={true}
         // eslint-disable-next-line @typescript-eslint/no-shadow
+        renderBubble={(props) => renderBubble(props)}
         onSend={(messages) => onSend(messages)}
         user={{
           _id: user.id,
           name: user.fullName,
           avatar: user.profileImageUrl,
         }}
+        renderSend={(props) => renderSend(props)}
         alwaysShowSend
+        scrollToBottomComponent={() => scrollToBottomComponent()}
         scrollToBottom
       />
 
@@ -75,51 +180,4 @@ const Meow = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#f5f5f5",
-  },
-  messageContainer: {
-    backgroundColor: "#ffffff",
-    borderRadius: 8,
-    marginVertical: 5,
-    marginHorizontal: 10,
-    padding: 10,
-    maxWidth: "70%",
-  },
-  messageText: {
-    fontSize: 15,
-  },
-  timestamp: {
-    fontSize: 15,
-    color: "gray",
-    marginTop: 5,
-    alignSelf: "flex-end",
-  },
-  inputContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    borderTopWidth: 1,
-    borderTopColor: "#cccccc",
-    backgroundColor: "#ffffff",
-    overflow: "hidden",
-  },
-  input: {
-    borderWidth: 1,
-    marginHorizontal: 10,
-    marginBottom: 10,
-    borderColor: "#cccccc",
-    borderTopEndRadius: 20,
-    borderTopStartRadius: 20,
-    borderBottomEndRadius: 20,
-    borderBottomStartRadius: 20,
-    overflow: "hidden",
-    borderBottom: 0,
-  },
-});
-
-export default Meow;
+export default withTheme(Meow);
